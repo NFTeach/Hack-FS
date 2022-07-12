4d695f7dd5fdd75beb4e0d93bf4938d8145d4bbd
Bryce Palichuk <bpalichu@ualberta.ca>
2022-07-12 16:03:37 -0600

    Add authentication, chains, and basic tabs

---
+++ .history/src/components/MenuItems_20220712153329.jsx
@@ -0,0 +1,29 @@
+import { useLocation } from "react-router";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+
+function MenuItems() {
+  const { pathname } = useLocation();
+
+  return (
+    <Menu
+      theme="light"
+      mode="horizontal"
+      style={{
+        display: "flex",
+        fontSize: "17px",
+        fontWeight: "500",
+        width: "100%",
+        justifyContent: "center",
+      }}
+      defaultSelectedKeys={[pathname]}
+    >
+      <Menu.Item key="/uploadcontent">
+        <NavLink to="/uploadcontent">⬆️ Upload Content</NavLink>
+      </Menu.Item>
+      
+    </Menu>
+  );
+}
+
+export default MenuItems;
--- .history/src/index_20220712101602.js
+++ .history/src/index_20220712101602.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
 import "./index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import UploadContent from "./components/UploadContent";
 
 /** Get your free Moralis Account https://moralis.io/ */
@@ -43,5 +43,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
