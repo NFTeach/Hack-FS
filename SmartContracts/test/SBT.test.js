7a5b69e12415cc91ef89b3fbbf9b7e823b8616f9
sraylr <ogjkerr@gmail.com>
2022-07-22 19:50:21 -0700

    improved testing

--- SmartContracts/test/SBT.test.js
+++ SmartContracts/test/SBT.test.js
@@ -1,10 +1,10 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 
-const uri = "https://gateway.pinata.cloud/ipfs/QmPho6v62jctLRGsZv2A8pyRGM4KCDvqDjKJbJm3sp4VUA/{id}.json";
+const uri = "https://gateway.moralisipfs.com/ipfs/";
 const mintPrice = ethers.utils.parseEther('0.001')
 
-describe('SBT Test', function () {
+describe('SBT Contract', function () {
     before(async function () {
         SBT = await ethers.getContractFactory("SBT");
         sbt = await SBT.deploy(uri);
@@ -12,117 +12,160 @@ describe('SBT Test', function () {
 
         [testOwner, testEducator, testStudent, testStudent2, testAttacker] = await ethers.getSigners();
     });
+
+    it('should have set correct owner on deploy', async function () {
+        assert.equal(await sbt.owner(), testOwner.address);
+    });
+
+    describe('Add educator', function () {
+        it('should revert if not owner', async function () {
+            await expect(sbt.connect(testAttacker).addEducator(testEducator.address)).to.be.revertedWith("Ownable: caller is not the owner"); 
+        });
+        
+        it('should add a new educator', async function () {
+            await sbt.addEducator(testEducator.address);
+            assert.isTrue(await sbt.isEducator(testEducator.address));
+        });
+
+        it('should revert if educator already exists', async function () {
+            await expect(sbt.addEducator(testEducator.address)).to.be.revertedWith("Educator already exists");
+        });
+    })
+
+    describe('Add student', function () {
+        it('should revert if not owner', async function () {
+            await expect(sbt.connect(testAttacker).addStudent(testStudent.address)).to.be.revertedWith("Ownable: caller is not the owner"); 
+        });
+        
+        it('should add a new student', async function () {
+            await sbt.addStudent(testStudent.address);
+            assert.isTrue(await sbt.isStudent(testStudent.address));
+        });
+
+        it('should revert if student already exists', async function () {
+            await expect(sbt.addStudent(testStudent.address)).to.be.revertedWith("Student already exists");
+        });
+    })
+
+    describe('Test creation', function () {
+        it('should revert if not educator', async function () {
+            await expect(sbt.connect(testAttacker).createSBT(mintPrice, uri)).to.be.revertedWith("Not an educator"); 
+        });
+
+        it('should create a new test', async function () {
+            assert.equal(await sbt.nbClassesCreated(testEducator.address), 0) // Get number of classes created by educator which should be 0
+            await sbt.connect(testEducator).createSBT(mintPrice, uri); // Create a class
+            assert.equal(await sbt.nbClassesCreated(testEducator.address), 1) // Update number of classes created which should now be 1
     
-    it('should deploy with correct owner', async function () {
-        // Deployed owner should be set correctly
-        expect(await sbt.owner()).to.equal(testOwner.address);
+            assert.equal(await sbt.getTestEducator(0), testEducator.address); // Educator should be set correctly
+        });
     });
 
-    it('should add a new educator as owner', async function () {
-        // Should revert due to not being the owner
-        await expect(sbt.connect(testAttacker).addEducator(testEducator.address)).to.be.revertedWith("Ownable: caller is not the owner"); 
+    describe('Test validation', function () {
+        it('should revert if not owner', async function () {
+            await expect(sbt.connect(testAttacker).validateStudentTest(testStudent.address, 0)).to.be.revertedWith("Ownable: caller is not the owner");
+        })
+
+        it('should revert if token does not exist', async function () {
+            await expect(sbt.validateStudentTest(testStudent.address, 394587)).to.be.revertedWith("Token doesn't exist");
+        })
+
+        it('should validate the students test', async function () {
+            classesCompleted = await sbt.nbClassesCompleted(testStudent.address); // Get number of classes completed by student
+            assert.equal(classesCompleted, 0); // which should be 0
+
+            nbCompleted = await sbt.nbTestCompletions(0); // Get number of times test has been completed
+            assert.equal(nbCompleted, 0) // which should be 0
+
+            await sbt.validateStudentTest(testStudent.address, 0); // Validate student test
+
+            classesCompleted = await sbt.nbClassesCompleted(testStudent.address); // Update classesCompleted
+            assert.equal(classesCompleted, 1); // which should now be 1
+
+            nbCompleted = await sbt.nbTestCompletions(0); // update nbCompleted
+            assert.equal(nbCompleted, 1) // which should now be 1
+
+            assert.isTrue(await sbt.isAllowedMint(testStudent.address, 0)); // Student should be allowedMint
+        })
+
+        it('should revert if student is already validated', async function () {
+            await expect(sbt.validateStudentTest(testStudent.address, 0)).to.be.revertedWith("Student already allowed to mint");
+        })
+    })
+
+    describe('Minting', function () {
+        it('should revert due to not being a student', async function () {
+            await expect(sbt.connect(testAttacker).mintSBT(0)).to.be.revertedWith("Not a student");
+        })
+
+        it('should revert due to incorrect msg.value', async function () {
+            await expect(sbt.connect(testStudent).mintSBT(0, { value: ethers.utils.parseEther('0.0001')})).to.be.revertedWith("Incorrect amount");
+        })
         
-        // Should succeed as owner
-        await sbt.addEducator(testEducator.address);
-        // and mapping should be updated
-        expect(await sbt.isEducator(testEducator.address)).to.equal(true);
-        
-        // Should revert due to educator already existing
-        await expect(sbt.addEducator(testEducator.address)).to.be.revertedWith("Educator already exists");
+        it('should revert due to not being validated by educator', async function () {
+            await sbt.addStudent(testStudent2.address); // create another student account which has not been validated
+            await expect(sbt.connect(testStudent2).mintSBT(0, { value: mintPrice})).to.be.revertedWith("Student is not allowed to mint this token");
+        })
+
+        it('should mint a token', async function () {
+            assert.equal(await sbt.connect(testStudent).balanceOf(testStudent.address, 0), 0) // get student token balance which should be 0
+            await expect(sbt.connect(testStudent).mintSBT(0, { value: mintPrice })).to.changeEtherBalance(testStudent, -mintPrice); // Mint a token and expect student's wallet value to decrease by mintPrice
+            assert.equal(await sbt.connect(testStudent).balanceOf(testStudent.address, 0), 1) // Update student token balance which should now be 1
+
+            assert.isFalse(await sbt.isAllowedMint(testStudent.address, 0)); // Student should not be allowedMint
+        })
+
+        it('should revert due to already having minted', async function () {
+            await expect(sbt.connect(testStudent).mintSBT(0, { value: mintPrice })).to.be.revertedWith('Student is not allowed to mint this token');
+        })
+
+    })
+
+    describe('Withdrawl', function () {
+        it('should revert due to not being an educator', async function () {
+            await expect(sbt.connect(testAttacker).withdrawCoursesPayoff()).to.be.revertedWith("Not an educator");
+        })
+
+        it('should withdraw the educators payout', async function () {
+            await expect(sbt.connect(testEducator).withdrawCoursesPayoff()).to.changeEtherBalance(testEducator, mintPrice); // withdraw and expect educator balance to increase
+        })
+
+        it('Should revert due to not having any payout left to withdraw', async function () {
+            await expect(sbt.connect(testEducator).withdrawCoursesPayoff()).to.be.revertedWith("No funds left to withdraw");
+        })
+    })
+})
+
+describe('Event Emitting', function () {
+    before(async function () {
+        SBT = await ethers.getContractFactory("SBT");
+        sbt = await SBT.deploy(uri);
+        await sbt.deployed(); 
+
+        [testOwner, testEducator, testStudent] = await ethers.getSigners();
     });
 
-    it('should add a new student as owner', async function () {
-        // Should revert due to not being the owner
-        await expect(sbt.connect(testAttacker).addStudent(testStudent.address)).to.be.revertedWith("Ownable: caller is not the owner"); 
-        
-        // Should succeed as owner
-        await sbt.addStudent(testStudent.address);
-        // and mapping should be updated
-        expect(await sbt.isStudent(testStudent.address)).to.equal(true);
-
-        // Should revert due to student already existing
-        await expect(sbt.addStudent(testStudent.address)).to.be.revertedWith("Student already exists");
-    });
-
-    it('should create a new test as educator', async function () {
-        // Should revert due to not being an educator
-        await expect(sbt.connect(testAttacker).createSBT(mintPrice, uri)).to.be.revertedWith("Not an educator");
-        
-        // Should succeed as educator
-        await sbt.connect(testEducator).createSBT(mintPrice, uri);
-        
-        // Educator's classesCreated should increment
-        expect(await sbt.nbClassesCreated(testEducator.address)).to.equal(1);
-        
-        // Test struct should have set educator correctly
-        expect(await sbt.getTestEducator(0)).to.equal(testEducator.address);
+    it('should emit an event when a new educator is added', async function() {
+        await expect(sbt.addEducator(testEducator.address)).to.emit(sbt, 'AddEducator');
+    })
+    
+    it('should emit an event when a new student is added', async function () {
+        await expect(sbt.addStudent(testStudent.address)).to.emit(sbt, 'AddStudent');
     })
 
-
-    it('should validate a students test as owner', async function () {
-        // Should revert due to not being owner
-        await expect(sbt.connect(testAttacker).validateStudentTest(testStudent.address, 0)).to.be.revertedWith("Ownable: caller is not the owner");
-        
-        // Should revert due to the token not existing
-        await expect(sbt.validateStudentTest(testStudent.address, 394587)).to.be.revertedWith("Token doesn't exist");
-
-        // Should succeed as owner
-        await sbt.validateStudentTest(testStudent.address, 0);
-        
-        // Student should be allowedMint
-        expect(await sbt.isAllowedMint(testStudent.address, 0)).to.equal(true);
-        
-        // Should revert due to student already having been validated 
-        await expect(sbt.validateStudentTest(testStudent.address, 0)).to.be.revertedWith("Student already allowed to mint");
-
-        // Student's classCompleted should increment
-        expect(await sbt.nbClassesCompleted(testStudent.address)).to.equal(1);
-        // Test's nbCompleted should increment
-        expect(await sbt.nbTestCompletions(0)).to.equal(1);
+    it('should emit an event when a class is created', async function () {
+        await expect(sbt.connect(testEducator).createSBT(mintPrice, uri)).to.emit(sbt, 'CreateTest');
     })
 
-    it('should mint a new token as student', async function () {
-        // Should revert due to not being a student
-        await expect(sbt.connect(testAttacker).mintSBT(0)).to.be.revertedWith("Not a student")
-
-        // create another student account which has not been validated
-        await sbt.addStudent(testStudent2.address);
-        // Should revert due to not being validated by owner
-        await expect(sbt.connect(testStudent2).mintSBT(0)).to.be.revertedWith("Student is not allowed to mint this token");
-
-        // Should revert due to incorrect msg.value
-        await expect(sbt.connect(testStudent).mintSBT(0, { value: ethers.utils.parseEther('0.0001')})).to.be.revertedWith("Incorrect amount");
-
-        // Should succeed as validated student with correct msg.value
-        await sbt.connect(testStudent).mintSBT(0, { value: mintPrice });
-
-        // Contract balance should increase
-        // Student's balance should decrease
-
-        // Student's token balance should increment
-        expect(await sbt.connect(testStudent).balanceOf(testStudent.address, 0)).to.equal(1);
-        // Student's sbtMinted should increment
-        expect(await sbt.nbMinted(testStudent.address)).to.equal(1);
-        // Educator's payout should increase
-        expect(await sbt.payout(testEducator.address)).to.equal(mintPrice);
-
-        // Student should not be allowedMint
-        expect(await sbt.isAllowedMint(testStudent.address, 0)).to.equal(false);
-        // Student should not be able to mint another token
-        await expect(sbt.connect(testStudent).mintSBT(0, { value: mintPrice })).to.be.reverted;
-
+    it('should emit an event when a students test is validated', async function () {
+        await expect(sbt.validateStudentTest(testStudent.address, 0)).to.emit(sbt, 'ValidateTest');
     })
 
-    it('should withdraw the educators payout', async function () {
-        // Should revert due to not being an educator
-        await expect(sbt.connect(testAttacker).withdrawCoursesPayoff()).to.be.revertedWith("Not an educator");
-        
-        // Should succeed as educator with payout remaining
-        await sbt.connect(testEducator).withdrawCoursesPayoff();
-
-        // Educator's balance should increase
-
-        // Should revert due to not having any payout left to withdraw
-        await expect(sbt.connect(testEducator).withdrawCoursesPayoff()).to.be.revertedWith("No funds left to withdraw");
+    it('should emit an event when a student mints a token', async function () {
+        await expect(sbt.connect(testStudent).mintSBT(0, { value: mintPrice })).to.emit(sbt, 'MintSBT');
     })
-});
+
+    it('should emit an event when an educator withdraws their payout', async function () {
+        await expect(sbt.connect(testEducator).withdrawCoursesPayoff()).to.emit(sbt, 'Withdrawl');
+    })
+})
